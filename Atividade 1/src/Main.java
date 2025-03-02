7d5a10cad65fa1af4e17d7d22edf58b8f469cd83
Nawan Anderson <87954530+NawanKK@users.noreply.github.com>
2025-03-01 21:47:19 -0400

    Add files via upload

---
+++ Atividade 1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[]args){
+        String nome = "Nawan";
+        String email = "nawan@gmail.com";
+            System.out.println("Olá " + nome+"!"+"Seu e-mail"+ email+ " Foi Cadastrado com Sucesso!");
+    }
+}
